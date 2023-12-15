efeb3ffcb8e750f529d067970476377290038707
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2023-12-15 18:40:57 +0530

    The styles done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='logo'>
+      <span className='logo'>Your Chat</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>John</span>
+        <button>logout</button>
+      </div>
+    </div>
   )
 }
 
